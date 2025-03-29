7522084b94d4b4ca2cf981b61878065ba217cb97
Rustam Shrestha <santosh.787402@smc.tu.edu.np>
2025-03-29 13:27:30 +0545

    made program not need extension explicitly in each return by making centralized configuration of jsp page extension

--- SpringBootWeb/src/main/java/org/sbinaction/SpringBootWeb/HomeController.java
+++ SpringBootWeb/src/main/java/org/sbinaction/SpringBootWeb/HomeController.java
@@ -4,21 +4,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "index.jsp"; // Spring Boot will look for index.html in the static directory
+        return "index"; // Spring Boot will look for index.html in the static directory
     }
     @RequestMapping("/add")
-    public String add(int number1, int number2,HttpSession session){
-//        int n1 =Integer.parseInt(request.getParameter("number1"));
-//        int n2 =Integer.parseInt(request.getParameter("number2"));
-        int resut = number1*number2;
-        System.out.println(resut);
-        session.setAttribute("result", resut);
-        return "result.jsp";
+    public String add(@RequestParam("number1") int number1, @RequestParam("number2") int number2, HttpSession session, Model model) {
+        int result = number1 * number2;
+
+        model.addAttribute("result", result);
+//    System.out.println(result);
+//    session.setAttribute("result", result);
+        return "result";
     }
+
 }
